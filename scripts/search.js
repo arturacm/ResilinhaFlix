a9d37245746008132e67c385ec4c706eadc407a2
Artur Camara Moreira <arturacm@gmail.com>
2021-05-11 22:26:38 -0300

    API apresenta na tela 10 posteres de filmes/series

--- scripts/search.js
+++ scripts/search.js
@@ -1,13 +1,37 @@
 let url = "http://www.omdbapi.com/?apikey=40078a35";
-
+var page = 0;
 
 function start(){
-    let search = $("#search").val()
-   
+    page = 1;
+    let search = $("#search").val().trim()
+    $(".searchResult").html("")
     console.log(search)
     $.ajax({url: url+"&s="+ search,
     success: (result)=>{
-        console.log(JSON.stringify(result))
+        console.log(result)
+        display(result);
+    },
+    error: ()=>{
+        alert("The selected day is not available")
+    }
+    })
+}
+
+function display(obj){
+    //obj.Search[i].title
+    for(let i = 0; i < obj.Search.length; i++){
+        //console.log(obj.Search[i].Title)
+        $(".searchResult").append(`<img src="${obj.Search[i].Poster}">`)
+            //obj.Search[i].Title
+    }
+}
+
+function nextPage(){
+
+    $.ajax({url: url+"&s="+ search,
+    success: (result)=>{
+        console.log(result)
+        display(result);
     },
     error: ()=>{
         alert("The selected day is not available")
