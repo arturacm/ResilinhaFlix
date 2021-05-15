261e009bc0a19920426b7ce3567ca790be65e3b4
Artur Camara Moreira <arturacm@gmail.com>
2021-05-15 14:51:12 -0300

    implementação dos carrosseis com imagens recebidas pela API

---
+++ src/font/model/modelMenuPageOMDB.js
@@ -0,0 +1,21 @@
+var url = "http://www.omdbapi.com/?apikey=40078a35";
+
+
+// Segunda parte: faz a requisição pela API e retorna o objeto
+function searchAPImainPage(search, location){
+    console.log(search, location)
+    $.ajax({url: url+"&s="+ search,
+    success: (result)=>{
+        console.log(result)
+        if (result.Response == "True"){
+            displayMenuPage(result, location);
+        }
+        else{
+            console.log("algo de errado nao esta certo")
+        }
+    },
+    error: ()=>{
+        alert("The selected day is not available")
+    }
+    })
+}
---
+++ src/font/views/menuPageSlick.js
@@ -0,0 +1,42 @@
+//Configurações do carrossel
+$('.items').slick({
+    centerMode: true,
+    centerPadding: '60px',
+    slidesToShow: 5,
+    slidesToScroll: 3,
+    variableWidth: true,
+    infinite:true,
+    dots: true,
+    focusOnSelect: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          centerMode: true,
+          centerPadding: '40px',
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          centerMode: true,
+          centerPadding: '40px'
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          centerMode: true,
+          centerPadding: '40px'
+        }
+      }
+  ]
+  });
