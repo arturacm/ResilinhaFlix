c7aeab029d8259a19a6cb2bc030c5d1480a06777
Artur Camara Moreira <arturacm@gmail.com>
2021-05-15 11:39:15 -0300

    acdionando titulo aos resultados de pesquisa

--- src/font/views/viewSearchOMDB.js
+++ src/font/views/viewSearchOMDB.js
@@ -6,7 +6,7 @@ function display(obj){
     
     for(let i = 0; i < obj.Search.length; i++){
         //console.log(obj.Search[i].Title)
-        $(".searchResult").append(`<img src="${obj.Search[i].Poster}" alt = "${obj.Search[i].Title} ">`)
+        $(".searchResult").append(`<div class="searchResultItem"><img src="${obj.Search[i].Poster}" alt = "${obj.Search[i].Title} "><h3>${obj.Search[i].Title} </h3></div>`)
             //obj.Search[i].Title
     }
 }
