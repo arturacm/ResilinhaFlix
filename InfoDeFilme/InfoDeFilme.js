3d9ac1a26c0a7c2273a473081685e6102c3d8cd0
unknown <allan.r.lima2017@gmail.com>
2021-05-17 22:31:23 -0300

    Adicionando mais informações sobre o filme e ajustes na página

--- InfoDeFilme/InfoDeFilme.js
+++ InfoDeFilme/InfoDeFilme.js
@@ -2,7 +2,7 @@ let btn = document.getElementById("botao1")
 
 btn.onclick = function () {
     let input = document.getElementById("input").value
-    let url = "http://www.omdbapi.com/?i=tt3896198&apikey=90a81d4f"
+    let url = "http://www.omdbapi.com/?apikey=90a81d4f"
 
     axios.get(url + "&t=" + input) 
     .then(function(response) {
@@ -10,9 +10,25 @@ btn.onclick = function () {
     let imagem = document.getElementById("imagem2")
     imagem.setAttribute("src", response.data.Poster)
 
+
+    let title = document.getElementById("titulo")
+    title.textContent = response.data.Title
+
+    let year = document.getElementById("ano")
+    year.textContent = 'lançamento: ' + response.data.Released
+
     let informacoes = document.querySelector("#info").firstChild
-    //let texto = document.createTextNode(response.data.Plot)
-    informacoes.textContent = response.data.Plot
+    informacoes.textContent = 'Descrição: ' + response.data.Plot
+
+    let rated = document.getElementById("classificacao")
+    rated.textContent = 'Classificação: ' + response.data.Rated
+
+    let genre = document.getElementById("genero")
+    genre.textContent = 'Gênero: ' + response.data.Genre
+    
+    let runtime = document.getElementById("duracao")
+    runtime.textContent = 'Duração: ' + response.data.Runtime
+
         
 })
 }
