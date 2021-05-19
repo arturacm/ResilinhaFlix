70b21f12c23787a95ed46be1a64b2a8889902c5d
Artur Camara Moreira <arturacm@gmail.com>
2021-05-19 14:22:18 -0300

    ajute do tamanho das imagens do carrossel

--- src/font/views/viewMenuPageOMDB.js
+++ src/font/views/viewMenuPageOMDB.js
@@ -3,7 +3,7 @@ function displayMenuPage(obj, location){
     console.log(location)
     for(let i = 0; i < obj.Search.length; i++){
         //console.log(obj.Search[i].Title)
-        $(location).slick('slickAdd',`<img ondblclick = "redirect('${obj.Search[i].imdbID}')"src="${obj.Search[i].Poster}" alt = "${obj.Search[i].Title} ">`)
+        $(location).slick('slickAdd',`<div class="imagens-slick" ondblclick = "redirect('${obj.Search[i].imdbID}')"><img src="${obj.Search[i].Poster}" alt = "${obj.Search[i].Title} "></div>`)
             //obj.Search[i].Title
     }
     console.log("got here")
