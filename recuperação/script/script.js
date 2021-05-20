c94723d4cda842644b8787885b2c63a57b6d16d1
Artur Camara Moreira <arturacm@gmail.com>
2021-05-19 23:26:51 -0300

    esqueci minha senha implementado

--- recuperação/script/script.js
+++ recuperação/script/script.js
@@ -1,2 +1,6 @@
 function insereTexto()
 {document.getElementById('divTeste').innerHTML = 'Redefinição de senha enviado para o email cadastrado.';}
+
+document.addEventListener("submit", ()=>{
+    alert("Foi enviado um email para redefinição de senha")
+})
